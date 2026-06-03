f355b544787f0847ad81cf4675f55db7a9e0aca5
Mohammad Kaif Shaikh <mohammadkaif6268@gmail.com>
2026-06-03 22:59:45 +0530

    Fixed Files Structure

--- Javascript/Phase - 2 Sheet 1/script.js
+++ Javascript/Phase - 2 Sheet 1/script.js
@@ -49,5 +49,195 @@
 
 
 // 8. Create a function to calculate area of rectangle.
+// function areaOfRectangle(w ,l){
+// return (w * l)
+// }
+// console.log(areaOfRectangle(10 , 20))
+
 // 9. Write a function that returns `"Adult"` if age ≥ 18 else `"Minor"`.
+// function isAdult(age){
+// if(age >= 18){
+//    return "Adult"
+// }else{
+//    return "Minor"
+// }
+// }
+// console.log(isAdult(18))
+
 // 10. Create a function to reverse a string.
+// function revString(str){
+//    var rev = ""
+// for(let i = str.length-1 ; i >= 0 ; i--){
+//    rev+=str[i]
+// }
+// return rev
+// }
+// console.log(revString("Alfiya"))
+
+
+
+
+// ## Intermediate Level
+
+// 1. Write a function expression for multiplication.
+// let multiplication = function(num , num1){
+// return num * num1
+// }
+// console.log(multiplication(5 , 10))
+
+// 2. Convert a normal function into an arrow function.
+// let multiplication = (num , num1) =>{
+// return num * num1
+// }
+// console.log(multiplication(5 , 10)).
+
+
+// 3. Create a function that accepts unlimited numbers and returns their sum using rest operator.
+// let multiplication = (...rest) =>{
+//    let num = 0
+// for(let i = 0 ; i <= rest.length-1 ; i++){
+//    num+=rest[i]
+// }
+// return num
+// }
+// console.log(multiplication(5,10 ,30,40,50,60,70))
+
+// 4. Write a function that counts vowels in a string.
+// function countVowels(str){
+//    let vowel = 0
+//    for(let i = 0 ; i < str.length ; i++){
+//       // console.log(i);/
+//       if(str[i] == "a" || str[i] == "e" ||str[i] == "i" ||str[i] == "o" ||str[i] == "u"){
+//          vowel++
+//       }
+//    }
+//    return vowel
+// }
+// console.log(countVowels("kaif"))
+
+// 5. Create a function that checks if a string is palindrome.
+// function isPalindrome(str){
+//    // console.log(str.length-1)
+//    let rev = ""
+//    for(let i = str.length-1 ; i >= 0 ; i--){
+//       rev+=str[i]
+//    }
+// // console.log(rev);
+// // console.log(str);
+//    if(rev == str) return true
+//    else return false
+// }
+// console.log(isPalindrome("madams"));
+
+
+// 6. Write a callback function example using `setTimeout`.
+// setTimeout(function(){
+//    console.log("Hello Im Soldier Boy")
+// },3000)
+
+// 7. Create a higher-order function that executes another function twice.
+// function higherOrder(call){
+// console.log("Hello Higher Order")
+// call()
+// call()
+// }
+
+// function callBack(){
+//    console.log("Hello Call Back")
+// }
+// higherOrder(callBack)
+
+
+// 8. Write a function that returns another function.
+// function higherOrder(call){
+// return call
+// }
+// function callBack(){
+//   console.log("executes")
+// }
+// console.log(higherOrder(callBack))
+
+// 9. Create a pure function for subtraction.
+// function subtraction(a,b){
+// return a - b
+// }
+// console.log(subtraction(10,50))
+
+// 10. Create an impure function using global variable modification.
+// var count = 0
+// function global(n){
+// for(let i  =  0 ; i < n ; i++){
+//    count++
+// }
+// return count
+// }
+
+// console.log(global(10))
+// console.log(global(10))
+
+
+// # Part 2 — Advanced Functions (21–35)
+
+// 1. Write a recursive function for factorial.
+// function factorial(n){
+// let result = 1
+// for(let i = 1 ; i <= n ; i++){
+//    result*=i
+// }
+// return result
+// }
+// console.log(factorial(6))
+
+// 2. Write recursive Fibonacci function.
+// function Fibonacci(a,b){
+// console.log(a+b)
+// }
+
+// Fibonacci(0 ,1)
+
+// 3. Create a function that finds power using recursion.
+// 4. Create an IIFE that prints `"Executed"`.
+// (function(){
+//    console.log("executes")
+// }())
+// 5. Write a function that memoizes factorial calculation.
+// 6. Create a closure counter function.
+// function counter(){
+//    let count = 0
+//    return function timer() {
+//   count+=1
+//   return count
+// }
+// }
+
+// let councct = counter()
+// console.log(councct())
+// console.log(councct())
+// console.log(councct())
+
+
+// 7. Write a function currying example for addition.
+// function currying(a){
+// return function(b) {
+//   return function(c) {
+//     return a + b + c
+//   }
+// }
+// }
+// console.log(currying(2)(5)(8))
+
+
+// 8. Create debounce function logic.
+// 9. Create throttle function logic.
+// 10. Write a function that executes only once.
+
+let hasExcuted = function(a){
+  return a + a
+}
+console.log(hasExcuted(5))
+console.log(hasExcuted(5))
+
+// 11. Create custom implementation of `map`.
+// 12. Create custom implementation of `filter`.
+// 13. Create custom implementation of `reduce`.
+// 14. Create custom `forEach`.
---
+++ Javascript/Phase - 2 Sheet 2/script.js
@@ -0,0 +1,22 @@
+// ## 1. Array
+
+// ### Intermediate
+
+// Create an array of 5 favorite movies and print all values.
+// let arr = ["3 Idiots" , "Off Campus" , "Sultan" , "BodyGuard" , "Heropanti"]
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+// console.log(arr[4])
+// for(let i = 0 ; i < arr.length ; i++){
+//    console.log(arr[i])
+// }
+
+// **Hint:** Use indexing
+
+// ### Hard
+
+// Create an array containing numbers, strings, boolean, and another array. Print only the nested array value.
+
+// **Hint:** Mixed data types + nested indexing
