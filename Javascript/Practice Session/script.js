4e4b086999c1c4dae350203602ebb695389d506f
Mohammad Kaif Shaikh <mohammadkaif6268@gmail.com>
2026-06-08 01:03:47 +0530

    Js Dsa Practice

--- Javascript/Practice Session/script.js
+++ Javascript/Practice Session/script.js
@@ -2,37 +2,159 @@
 // let result = price.filter((elem)=>{
 //  return elem > 300
 // })
-
 // console.log(result)
 
-
+// Question - 2
 // let marks = [80,90,70,85,95]
-// let sum = marks.reduce((acc , val) => acc += val ) 
+// let sum = marks.reduce((acc , val) => acc += val )
 // let average = sum/marks.length
 // console.log(average)
 
-
 // Question - 3
 
-let numbers= [1,2,3,2,4,2,5,1,1,1,2,2,2,2];
-var obj = {}
+// let numbers = [1, 2, 3, 2, 4, 2, 5, 1, 1, 1, 2, 2, 2, 2];
+// var obj = {};
+// for (let i = 0; i <= numbers.length - 1; i++) {
+//   // console.log(numbers[i])
+//   if (obj[numbers[i]]) {
+//     obj[numbers[i]] += 1;
+//   } else {
+//     obj[numbers[i]] = 1;
+//   }
+// }
+// // console.log(obj)
+// let frequency = 0;
+// let mostFrequecy = 0;
+// for (let key in obj) {
+//   // console.log(obj[key])
+//   if (obj[key] > frequency) {
+//     frequency = obj[key];
+//     mostFrequecy = key;
+//   }
+// }
+// console.log(mostFrequecy);
 
-for(let i = 0 ; i <= numbers.length-1 ; i++){
-// console.log(numbers[i])
-if(obj[numbers[i]]){
-   obj[numbers[i]] += 1
-}else{
-  obj[numbers[i]] = 1
+// Question - 4
+
+// let user= {
+// name:"Ritik",
+// age:20
+// };
+// user.age = 21
+// console.log(user)
+
+// Question - 5
+
+// let user= {
+// name:"Ritik",
+// age:20,
+// city:"Bhopal"
+// };
+// let rest = Object.entries(user)
+// for(let [key , value] of rest){
+//    console.log(value)
+// }
+
+// Question - 6
+
+// let employees = {
+//    aman: 25000,
+//    ritik: 50000,
+//    priya: 45000
+// };
+// let salary = 0
+// // let obj = {}
+// let obj = []
+// for(let key in employees){
+//    // console.log(key , employees[key])
+//    if(employees[key] > salary){
+//       salary = employees[key]
+//       // obj = {name : key,
+//       //    salary : employees[key]
+//       // }
+//        obj =[key ,employees[key]]
+//    }
+// }
+// // console.log(obj.name)
+// console.log(obj[0])
+
+// Question - 7
+
+// function greet(name){
+// return `Hello ${name}`
+// }
+// console.log(greet("Kaif"))
+
+// Question - 8
+
+// function calculateDiscount(price){
+// let discount = price * (10 / 100)
+// let finalPrice = price - discount
+// return finalPrice
+// }
+
+// console.log(calculateDiscount(500))
+
+// Question - 9
+
+// function sum(...numbers){
+// return numbers.reduce((acc , value) => acc+=value , 0)
+// }
+
+// console.log(sum(1,2,3,4,5))
+
+// Question - 10
+// let users= [
+// { name:"Ritik", age:20 },
+// { name:"Aman", age:16 },
+// { name:"Priya", age:25 }
+// ];
+// function getAdults(user){
+// let ans = user.filter((elem)=> elem.age >= 18)
+// return ans
+// }
+// console.log(getAdults(users))
+
+// Question - 11
+// let cart= [
+// { name:"Mouse", price:500, qty:2 },
+// { name:"Keyboard", price:1000, qty:1 },
+// { name:"Monitor", price:10000, qty:1 }
+// ];
+
+// function getCartTotal(cart){
+//    let total = cart.reduce((acc , elem) =>{
+//         return acc+= elem.price * elem.qty
+//    },0)
+//    return total
+// }
+// console.log(getCartTotal(cart))
+
+
+// Question - 12
+
+let students = [
+  {
+    name: "Ritik",
+    marks: [80, 90, 85],
+  },
+  {
+    name: "Aman",
+    marks: [50, 40, 60],
+  },
+];
+
+
+function generateReport(students){
+let obj = {}
+let totalMarks ;
+for(let i = 0 ; i <= students.length-1 ; i++){
+   // console.log(students[i].marks)
+   tstudents[i].marks.reduce((acc,val)=>{
+      return acc+=val
+   },0)
 }
+return totalMarks
 }
-// console.log(obj)
-let frequency = 0
-let mostFrequecy = 0
-for(let key in obj){
-   // console.log(obj[key])
-   if(obj[key] > frequency){
-      frequency = obj[key]
-      mostFrequecy = key
-   }
-}
-console.log(mostFrequecy)
+
+console.log(generateReport(students))
