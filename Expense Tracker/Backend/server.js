f1ae49f78e6868c2540573d819655c7fcd081ff1
Mohammad Kaif Shaikh <mohammadkaif6268@gmail.com>
2026-09-22 17:12:38 +0530

    Connect Db

--- Expense Tracker/Backend/server.js
+++ Expense Tracker/Backend/server.js
@@ -1,8 +1,10 @@
+require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"])
+
 const dotenv = require('dotenv')
 
 dotenv.config()
-
 const app = require('./src/app')
+
 
 const PORT = process.env.PORT || 5000
 
--- Expense Tracker/Backend/src/app.js
+++ Expense Tracker/Backend/src/app.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const connectDB = require('./config/db')
 
 const app = express()
 
 app.use(express.json())
 
+connectDB()
+
+
+app.get('/' ,(req,res) => {
+   res.send("You Are In Page")
+})
 
 module.exports = app
