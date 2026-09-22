abb383d6a2993eb372e55315f8ac4d992fc9844c
Mohammad Kaif Shaikh <mohammadkaif6268@gmail.com>
2026-09-22 15:59:01 +0530

    Creating Expense Tracker App With React Node DB

---
+++ Expense Tracker/Backend/server.js
@@ -0,0 +1,12 @@
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const app = require('./src/app')
+
+const PORT = process.env.PORT || 5000
+
+
+app.listen(PORT, () => {
+   console.log(`Server Is Running On This Port ${PORT}`)
+})
